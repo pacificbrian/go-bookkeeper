c5ac20b041c5c1890f302c38a501e6dec2b0c3a1
Brian Welty <bkwelty@gmail.com>
2022-09-18 23:27:09 -0700

    Add support for Account#Index icons!

--- model/account_type.go
+++ model/account_type.go
@@ -15,6 +15,36 @@ type AccountType struct {
 	Name string `form:"account_type.Name"`
 }
 
+func (a AccountType) GetAltText() string {
+	return a.Name
+}
+
+func (a AccountType) GetIconPath() string {
+	var path string
+	switch a.Name {
+	case "Cash":
+		path = "images/icons/icn_small_deposit.png"
+	case "Checking/Deposit":
+		path = "images/icons/icn_small_deposit.png"
+	case "Credit Card":
+		path = "images/icons/icn_small_credit_card.gif"
+	case "Investment":
+		path = "images/icons/icn_investments.png"
+	case "Loan":
+		path = "images/icons/icn_home.png"
+	case "Health Care":
+		path = "images/icons/icn_health.png"
+	case "Asset":
+		path = "images/icons/icn_home.png"
+	case "Crypto":
+		path = "images/icons/bitcoin.png"
+	default:
+		path = "images/icons/wrench.svg"
+	}
+
+	return path
+}
+
 func (*AccountType) List(db *gorm.DB) []AccountType {
 	entries := []AccountType{}
 	db.Find(&entries)
