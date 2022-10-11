5bbc72fb6aea33225c7a201bec1a33d44f628800
Brian Welty <bkwelty@gmail.com>
2022-10-10 23:18:49 -0700

    webpack: Add babel loader as is needed for use of RXJS
    Much thanks! to this page:
    https://www.robinwieruch.de/webpack-babel-setup-tutorial
    
    While here, fix the webpack configs which were accidentally
    switched for development vs production.

--- javascript/webpack.config.js
+++ javascript/webpack.config.js
@@ -2,8 +2,20 @@ const path = require('path');
 
 module.exports = {
   entry: './javascript/client.js',
+  // https://www.robinwieruch.de/webpack-babel-setup-tutorial/
+  // https://babeljs.io/docs/en/babel-plugin-proposal-class-properties
+  module: {
+    rules: [{
+      test: /\.(js)?$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    }]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
   output: {
     path: path.resolve(__dirname, '../public/javascript'),
     filename: 'bundle.js',
-  },
+  }
 };
